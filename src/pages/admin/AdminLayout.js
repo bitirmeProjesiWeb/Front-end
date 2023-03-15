4f08e864a4449eb21fc206bd838f6633c581bb1a
f6c5 <cicekf256@gmail.com>
2023-03-15 19:10:04 +0300

    admin layouta outlet eklendi güzel oldu yani

--- src/pages/admin/AdminLayout.js
+++ src/pages/admin/AdminLayout.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function AdminLayout() {
   return (
-    <div>AdminLayout</div>
-  )
+    <div>
+      <Outlet />
+    </div>
+  );
 }
