f1e9b67e870425bd393cc930c4de9352007f2be5
f6c5 <cicekf256@gmail.com>
2023-03-15 23:17:22 +0300

    user homepage ufak bir hata giderildi

--- src/pages/user/HomePage.js
+++ src/pages/user/HomePage.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useData } from '../../context/Context';
 
 export default function HomePages() {
-  const { items } = useData();
   return (
     <div>
     </div>
