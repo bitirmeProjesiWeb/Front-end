0a2c2fdc79e2ca7de4d9eec065dcaa1ad703b200
f6c5 <cicekf256@gmail.com>
2023-03-15 23:15:56 +0300

    homepage ufak bir hata giderildi

--- src/pages/user/HomePage.js
+++ src/pages/user/HomePage.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import CardCom from '../../components/user/CardCom'
 import { useData } from '../../context/Context';
 
 export default function HomePages() {
   const { items } = useData();
   return (
-    <div style={{display:"flex"}}>
-      {items.map((card) => (
-        <CardCom {...card} />
-      ))}
+    <div>
     </div>
   )
 }
