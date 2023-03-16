1bfeb5762aa98b05496f42953e74e07760cf248c
f6c5 <cicekf256@gmail.com>
2023-03-16 15:47:17 +0300

    sidenav yer değişikliğinden kaynaklı hata gideerildi

--- src/pages/common/CommonLayout.js
+++ src/pages/common/CommonLayout.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavbarCom from "../../components/common/NavbarCom";
 import FooterCom from "../../components/common/FooterCom";
 import { Outlet } from "react-router-dom";
-import Sidenav from "../../components/common/Sidenav";
+import Sidenav from "../../components/user/Sidenav";
 
 export default function CommonLayout() {
   return (
