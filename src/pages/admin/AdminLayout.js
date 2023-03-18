00f1777df7b65ba283d3961e757a09b67937ee28
f6c5 <cicekf256@gmail.com>
2023-03-18 21:46:49 +0300

    admin layoutuna sidebar eklendi

--- src/pages/admin/AdminLayout.js
+++ src/pages/admin/AdminLayout.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import NavbarCom from "../../components/user/NavbarCom";
+import FooterCom from "../../components/common/FooterCom";
 import { Outlet } from "react-router-dom";
+import Sidenav from "../../components/admin/Sidenav";
 
 export default function AdminLayout() {
+  const [open, setOpen] = useState(false);
+  const [drawer, setDrawer] = useState({});
+
+  useEffect(() => {
+    open
+      ? setDrawer({
+          width: "250px",
+          transition: "0.27s",
+        })
+      : setDrawer({
+          width: "60px",
+          transition: "0.27s",
+          overflowX: "hidden",
+        });
+  }, [open]);
+
   return (
     <div>
-      <Outlet />
+      <NavbarCom drawer={drawer} open={open} setOpen={setOpen} />
+      <Sidenav drawer={drawer} open={open} setOpen={setOpen}>
+        <Outlet />
+        <FooterCom />
+      </Sidenav>
     </div>
   );
 }
