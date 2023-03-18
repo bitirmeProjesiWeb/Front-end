25aeee53b788814631662f0b0ea52a803af78f7d
f6c5 <cicekf256@gmail.com>
2023-03-18 17:04:08 +0300

    sidebar ve navbar common layouta eklendi

--- src/pages/common/CommonLayout.js
+++ src/pages/common/CommonLayout.js
@@ -1,14 +1,30 @@
-import React from "react";
-import NavbarCom from "../../components/common/NavbarCom";
+import React, { useEffect, useState } from "react";
+import NavbarCom from "../../components/user/NavbarCom";
 import FooterCom from "../../components/common/FooterCom";
 import { Outlet } from "react-router-dom";
 import Sidenav from "../../components/user/Sidenav";
 
 export default function CommonLayout() {
+  const [open, setOpen] = useState(false);
+  const [drawer, setDrawer] = useState({});
+
+  useEffect(() => {
+    open
+      ? setDrawer({
+          width: "250px",
+          transition: "0.27s",
+        })
+      : setDrawer({
+          width: "60px",
+          transition: "0.27s",
+          overflowX: "hidden",
+        });
+  }, [open]);
+
   return (
     <div>
-      <Sidenav>
-        <NavbarCom />
+      <NavbarCom drawer={drawer} open={open} setOpen={setOpen} />
+      <Sidenav drawer={drawer} open={open} setOpen={setOpen}>
         <Outlet />
         <FooterCom />
       </Sidenav>
