661337a9ededb0dbd5011d24529e54495ace217c
f6c5 <cicekf256@gmail.com>
2023-03-21 12:48:06 +0300

    useparams hooku eklendi

--- src/pages/user/UPitchDetailPage.js
+++ src/pages/user/UPitchDetailPage.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import CardCom from "../../components/common/CardCom";
+import { useData } from "../../context/Context";
 
 export default function UPitchDetailPage() {
+  const { pitchId } = useParams();
+  const { items } = useData();
+  const pitchData = items.find((item) => item.pitchId === parseInt(pitchId));
   return (
-    <div>UPitchDetailPage</div>
-  )
+    <div>
+      <CardCom key={pitchId} {...pitchData}></CardCom>
+    </div>
+  );
 }
