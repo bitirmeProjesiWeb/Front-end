c64b1c381d0402c7628a86c4e315e6aee6597b7c
f6c5 <cicekf256@gmail.com>
2023-03-29 20:27:55 +0300

    datadan bilgiler çekildi

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,24 +3,44 @@ import { createContext, useContext, useEffect, useState } from "react";
 const Context = createContext();
 
 const Provider = ({ children }) => {
-  const [items, setItems] = useState([]);
-
+  const [pitches, setPitches] = useState([]);
   const [user, setUser] = useState();
+  const [cities, setCities] = useState();
+  const [reservations, setReservations] = useState();
+  const [data, setData] = useState();
 
+  //************************************************** şehirler **************************************************
   useEffect(() => {
-
     const getirData = async () => {
-      const result = await axios("/data.json");
-      setItems(result.data.pitches);
+      const result = await axios.get(
+        "https://raw.githubusercontent.com/f6c5/world/master/turkiye-il-ilce.json"
+      );
+      setCities(result.data.states);
     };
     getirData();
-  }, []);
+  }, [!cities]);
+
+  useEffect(() => {
+    const getData = () => {
+      axios
+        .get("http://localhost:3000/data.json")
+        .then((data) => setData(data.data));
+    };
+    getData();
+    if (data) {
+      setPitches(data.pitches);
+      setReservations(data.reservations);
+    }
+  }, [!data,pitches,reservations]);
 
   const values = {
-    items,
-    setItems,
+    pitches,
+    setPitches,
     user,
     setUser,
+    cities,
+    reservations,
+    setReservations,
   };
 
   return <Context.Provider value={values}>{children}</Context.Provider>;
