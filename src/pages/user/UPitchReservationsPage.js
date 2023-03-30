16d987fb7df89ce6f2972eda2f4e3f95266ce8b6
f6c5 <cicekf256@gmail.com>
2023-03-30 18:03:30 +0300

    seanslar tarihe göre düzenleme eklendi

--- src/pages/user/UPitchReservationsPage.js
+++ src/pages/user/UPitchReservationsPage.js
@@ -1,4 +1,4 @@
-import { Button, Grid, Paper } from "@mui/material";
+import { Button, Grid, Paper, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useData } from "../../context/Context";
@@ -12,25 +12,59 @@ export default function UPitchReservationsPage() {
   useEffect(() => {
     const pitch = pitches.find((item) => item.pitchId === parseInt(pitchId));
     const sessions = pitch?.sessions;
+
     setFiltered(
       sessions?.filter(
         (session) =>
-          !reservations.some(
-            (reservation) => reservation.sessionId === session.sessionId
-          )
+          !reservations.some((reservation) => reservation.date === tarih)
       )
     );
     console.log(filtered);
   }, [!filtered, reservations, pitches]);
+
+  const [tarih, setTarih] = useState("");
+
+  const minTarih = new Date().toISOString().slice(0, 10);
+  const maxTarih = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+    .toISOString()
+    .slice(0, 10);
+
+  function tarihDegistir(event) {
+    const secilenTarih = event.target.value;
+
+    if (secilenTarih >= minTarih && secilenTarih <= maxTarih) {
+      setTarih(secilenTarih);
+    } else {
+      setTarih();
+    }
+  }
+
+  console.log(tarih);
   return filtered ? (
     <Grid container>
-      <Grid xs={1} margin={"1rem"} item>
-        {filtered.map((item) => (
-          <Button variant="outlined" key={item.sessionId} color="info">
-            {item.sessionStart} - {item.sessionFinish}
-          </Button>
-        ))}
-      </Grid>
+      <TextField
+        type="date"
+        value={tarih}
+        onChange={tarihDegistir}
+        inputProps={{
+          min: minTarih,
+          max: maxTarih,
+          style: {
+            color: tarih < minTarih ? "red" : "inherit",
+          },
+        }}
+      />
+
+      {tarih && (
+        <Grid xs={1} margin={"1rem"} item>
+        <Typography>seanslar:</Typography>
+          {filtered.map((item) => (
+            <Button variant="outlined" key={item.sessionId} sx={{marginTop:"1rem"}} color="info">
+              {item.sessionStart} - {item.sessionFinish}
+            </Button>
+          ))}
+        </Grid>
+      )}
     </Grid>
   ) : (
     <BackdropComp />
