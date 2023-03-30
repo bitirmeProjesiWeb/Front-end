594124c69e145c63e9ba457ddef0d830df14490c
f6c5 <cicekf256@gmail.com>
2023-03-30 17:59:47 +0300

    ziyaretçi için topbar oluşturuldu

---
+++ src/components/common/CNavbarCom.js
@@ -0,0 +1,64 @@
+import {
+  DarkModeOutlined,
+  LightModeOutlined,
+  NotificationsOutlined,
+  PersonOutlined,
+  SettingsOutlined,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  CssBaseline,
+  IconButton,
+  Toolbar,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { ColorModeContext, tokens } from "../../theme";
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+
+export default function CNavbarCom() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <Box sx={{ display: "flex", justifyContent: "space-between", p: "2px" }}>
+      <CssBaseline />
+      <AppBar
+        position="absolute"
+        color="inherit"
+        sx={{ background: colors.primary[400] }}
+      >
+        <Toolbar>
+          <Typography variant="h1" noWrap sx={{ flexGrow: 1 }}>
+            SporLife
+          </Typography>
+          <Box display="flex">
+            <IconButton onClick={colorMode.toggleColorMode}>
+              {theme.palette.mode === "dark" ? (
+                <DarkModeOutlined />
+              ) : (
+                <LightModeOutlined />
+              )}
+            </IconButton>
+            <IconButton>
+              <Badge badgeContent={4}>
+                <NotificationsOutlined />
+              </Badge>
+            </IconButton>
+            <IconButton>
+              <SettingsOutlined />
+            </IconButton>
+            <IconButton component={NavLink} to="/profile">
+              <PersonOutlined />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Box sx={{ flexGrow: 1, p: 3 }} />
+    </Box>
+  );
+}
