af1b4a122cafe273c1b774fbbe6a03198cd9ba89
f6c5 <cicekf256@gmail.com>
2023-03-30 18:03:43 +0300

    çıkışyap butonu eklendi

--- src/pages/user/UProfilePage.js
+++ src/pages/user/UProfilePage.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import { Button, Grid } from "@mui/material";
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useData } from "../../context/Context";
 
 export default function ProfilePages() {
+  const { logoutHandle } = useData();
   return (
-    <div>ProfilePages</div>
-  )
+    <Grid container>
+      <Grid item justifyContent={"center"} flexGrow={"1"}>
+        <Button
+          component={NavLink}
+          to="/"
+          onClick={logoutHandle}
+          variant="outlined"
+          color="info"
+        >
+          çık
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
