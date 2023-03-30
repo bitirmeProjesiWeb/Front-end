d31aa5d875ba536241d26ebad9c4f5e413e3864b
f6c5 <cicekf256@gmail.com>
2023-03-30 18:02:11 +0300

    login sayfası düzenlendi

--- src/pages/common/LoginPage.js
+++ src/pages/common/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import {
   TextField,
   Button,
@@ -8,82 +8,93 @@ import {
   Container,
   Typography,
 } from "@mui/material";
+import { useData } from "../../context/Context";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { loginHandle } = useData();
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     //todo login fonksiyonu
+    if (email && password) {
+      loginHandle({ email, password })
+        ? navigate("/")
+        : alert("böyle bir hesap bulunamadı");
+    }
   };
 
   return (
-      <Container component="main" maxWidth="xs">
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Typography component="h1" variant="h5">
-            Giriş Yap
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h2">
+          Giriş Yap
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            color="warning"
+            required
+            fullWidth
+            label="Email Adresi"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            color="warning"
+            required
+            fullWidth
+            label="Şifre"
+            type="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            disabled={!email || !password}
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            color="info"
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Adresi"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Şifre"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <Button
-              disabled={!email || !password}
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Giriş Yap
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <NavLink href="#" variant="body2">
-                  Şifreni mi unuttun?
-                </NavLink>
-              </Grid>
-              <Grid item>
-                <NavLink to="/register" variant="body2">
-                  Hesabın yok mu? Kayıt Ol
-                </NavLink>
-              </Grid>
+            Giriş Yap
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Typography
+                component={NavLink}
+                to="/#"
+                sx={{ textDecoration: "none", color: "inherit" }}
+              >
+                Şifreni mi unuttun?
+              </Typography>
             </Grid>
-          </Box>
+            <Grid item>
+              <Typography
+                component={NavLink}
+                to="/register"
+                sx={{ textDecoration: "none", color: "inherit" }}
+              >
+                Hesabın yok mu? Kayıt Ol
+              </Typography>
+            </Grid>
+          </Grid>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
 }
