dd97a16098bc29b0da325aabfa2fa92b10a44ddc
Sevval Senel <sevvalsenel@bssyazilim.com.tr>
2023-03-31 03:00:14 +0300

    içerik ve konum düzenlemesi

--- src/components/admin/ACapacityBox.js
+++ src/components/admin/ACapacityBox.js
@@ -4,34 +4,34 @@ import { ResponsivePie } from "@nivo/pie";
 import { useData } from "../../context/Context";
 import ACapacityChart from "./ACapacityChart";
 
-const ACapacityBox = ({ title, subtitle}) => {
+const ACapacityBox = ({ title, subtitle, increase}) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const { aCapacityData} = useData();
   return (
     <Box width="100%" m="0 30px">
       <Box display="flex" justifyContent="space-between">
-        <Box>
+        <Box
+       mt="20px"
+        >
           <Typography
-            variant="h6"
-            fontWeight="bold"
+            variant="h5"
+            fontWeight="600"
             sx={{ color: colors.grey[100] }}
           >
             {title}
           </Typography>
         </Box>
       </Box>
-      <Box display="flex" justifyContent="space-between" mt="2px">
+      <Box display="flex" justifyContent="space-between" mt="20px">
         <Typography variant="h6" sx={{ color: colors.greenAccent[500] }}>
           {subtitle}
         </Typography>
-        {/* <Typography
-          variant="h9"
-          fontStyle="italic"
-          sx={{ color: colors.greenAccent[600] }}
-        >
+      </Box>
+      <Box display="flex" justifyContent="space-between" mt="20px">
+        <Typography variant="h6" sx={{ color: colors.greenAccent[500] }}>
           {increase}
-        </Typography> */}
+        </Typography>
       </Box>
     </Box>
   );
