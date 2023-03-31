024f35c7e30b0b5f6ab917ad65780176009d6573
Sevval Senel <sevvalsenel@bssyazilim.com.tr>
2023-03-31 03:09:37 +0300

    TAMAMLANMAMIŞTIR

--- src/components/common/FooterCom.js
+++ src/components/common/FooterCom.js
@@ -1,7 +1,21 @@
+import { Box, Typography, useTheme } from '@mui/material'
 import React from 'react'
+import { tokens } from "../../theme";
 
 export default function FooterCom() {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <div>FooterCom</div>
+    <Box
+    sx={{  backgroundColor: colors.primary[400] }}
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+    height="60px"
+    >
+  <Typography variant="h6">
+        © 2023 SportLife | Tüm hakları saklıdır.
+      </Typography>
+    </Box>
   )
 }
