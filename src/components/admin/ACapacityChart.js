d075c2955ea1cfa44cf66ba6db0c482c41029a0f
Sevval Senel <sevvalsenel@bssyazilim.com.tr>
2023-05-29 15:45:41 +0300

    yüzdelik chartı ortalandı

--- src/components/admin/ACapacityChart.js
+++ src/components/admin/ACapacityChart.js
@@ -10,7 +10,7 @@ const ACapacityChart = ({
   return (
     <ResponsivePie
       data={aCapacityData}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+      margin={{ top: 40, right: 80, bottom: 40, left: 80 }}
       startAngle={-90}
       endAngle={90}
       innerRadius={0.5}
